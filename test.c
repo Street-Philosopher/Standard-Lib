e5b6cfc99fd815cfc8fd40a59e8509e50c685f83
Street-Philosopher <leoniantonio0@gmail.com>
2025-10-26 21:12:45 +0100

    gitignore

--- test.c
+++ test.c
@@ -3,15 +3,5 @@
 
 int _start() {
 
-	char outc[10];
 
-	for (int i = 0; i < 10; i++) {
-		outc[i] = getc();
-	}
-
-	print_nolen("now going to do the thing\n");
-
-	print(&outc, 10);
-
-	exit(0);
 }
