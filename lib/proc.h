5031d8196ebad6f9aa118b2395fb608b36b782dc
Street-Philosopher <leoniantonio0@gmail.com>
2025-11-21 20:14:37 +0100

    todo

--- lib/proc.h
+++ lib/proc.h
@@ -5,3 +5,4 @@ int vfork();
 int execve(char* file, char* argv[], char* envp[]);
 int wait4(int upid, int* stat_addr, int options, void* rusage);
 int kill(int pid, int sig);
+//TODO: raise() or kill(), signal(), sigaction()
